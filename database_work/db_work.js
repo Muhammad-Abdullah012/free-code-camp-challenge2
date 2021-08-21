7e37005e6d27f810d81f928b7039f9588b3bb17e
Abdullah <ma9589094@gmail.com>
2021-08-21 20:57:21 +0500

    Adding files

---
+++ database_work/db_work.js
@@ -0,0 +1,53 @@
+const createTable = (db,name) => {
+    db.schema.hasTable(name).then((exist) => {
+        if(!exist) {
+            db.schema.createTable(name, (table)=> {
+                table.increments('id').primary();
+                table.string("urls").unique("urls");
+            })
+            .catch(err => {
+                console.error(err);
+            })
+        }
+    })
+    .catch(err => {
+        console.error(err);
+    })
+}
+
+const insertInTable = async (db, url) => {
+    let shortURL = null;
+    shortURL = await getUrl(db, url);
+    if(!shortURL || shortURL.length === 0) {
+        db.insert({urls: url})
+        .into("shortUrls")
+        .catch(err => {
+            console.error(err);
+        })
+        return insertInTable(db,url);
+    }
+    else {
+        return shortURL;
+    }
+}
+const getUrl = async (db, url) => {
+    return await db.select("*").from("shortUrls").where("shortUrls.urls", "=", url).then(shUrl => {
+        return shUrl[0];
+    }).catch(err => {
+        console.error(err);
+    })
+}
+const getUrlById = async (db, id) => {
+    return await db.select("*").from("shortUrls").where("shortUrls.id", "=", id).then(shUrl => {
+        return shUrl[0];
+    }).catch(err => {
+        console.error(err);
+    })
+}
+
+module.exports = {
+    createTable,
+    insertInTable,
+    getUrl,
+    getUrlById
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const knex = require('knex');
+const bodyParser = require('body-parser');
+const {createTable, insertInTable, getUrl, getUrlById} = require('./database_work/db_work');
+const dns = require('dns');
+
+
+const app = express();
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+   filename: './database_files/data.db3' //or .sqlite3 are extensions of sqlite
+ },
+ useNullAsDefault: true
+});
+const options = {
+  family: 0,
+  hints: dns.ADDRCONFIG | dns.V4MAPPED,
+};
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+createTable(db, "shortUrls");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.post('/api/shorturl/', (req, res) => {
+  const url = req.body.url.split('//www.');
+  const index = url.length - 1;
+  if(index === 0) {
+    res.json({error: "Invalid URL"});
+  }
+  dns.lookup(url[index], options , async (err,address,family) => {
+    if(!err) {
+      let shortUrl = await insertInTable(db,req.body.url);
+      res.json({ original_url: shortUrl.urls, short_url: shortUrl.id });
+    }
+    else {
+      res.json({error: "Invalid URL"});
+    }
+  });
+});
+
+app.get('/api/shorturl/:url', async (req,res) => {
+  let url = req.params.url;
+  let OriginalUrl = await getUrlById(db, url);
+  if(!OriginalUrl || OriginalUrl.length === 0) {
+    res.json({error: "No short URL found for the given input"});
+  }
+  res.redirect(`${OriginalUrl.urls}`);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
